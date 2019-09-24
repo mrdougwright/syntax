e0d83df1514e64eff8a7249c6b830f66abd9a6e8
Doug Wright <mrdougwright@gmail.com>
2019-09-24 12:36:43 -0700

    Todo list app in react: ep 5

--- episode_5/app/src/App.js
+++ episode_5/app/src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoApp extends React.Component {
+  state = { items: [], text: '' }
+
+  handleChange = (e) => (
+    this.setState({ text: e.target.value })
+  )
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (!this.state.text.length) {
+      return
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    }
+
+    return this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default TodoApp
